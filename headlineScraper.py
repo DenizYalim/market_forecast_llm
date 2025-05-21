e66b0642b33985b07785705678098952abc84848
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-05-22 02:58:59 +0300

    new wip files

--- headlineScraper.py
+++ headlineScraper.py
@@ -3,4 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
-print("scraper")
+from config import SITES_TO_SCRAPE
+
+
+print(SITES_TO_SCRAPE)
+
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("main")
+
+# LINKS = 
+# Scrape headlines
+# headlines = {}
+# Send headlines to llm, get stock analysis
+# stock analysis = {stock:string, tendency:{DECREASE, INCREASE}}
