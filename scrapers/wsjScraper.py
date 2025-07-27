4be95ac9b34b6ccb6b34cd35953a6457fef8aef7
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-07-28 00:26:58 +0300

    added binanceWrapper.py to start with binance stuff, just pasted some gpt slop, it is 1 am and Im tired

--- scrapers/wsjScraper.py
+++ scrapers/wsjScraper.py
@@ -4,18 +4,19 @@ from selenium.webdriver.common.by import By
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
+# wsj blocks.
 def scrape_wsj_headlines(): 
     options = webdriver.ChromeOptions()
     # options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) # Maybe turn this into firefox
 
     driver.get("https://www.wsj.com/")
-    time.sleep(3)  # let page load
+    time.sleep(5)  # let page load
 
-    headline_element_XPATH = "//h3[contains(@class, 'e1sf124z8 css-1bx5v3n-HeadlineTextBlock')]"
+    headline_element_XPATH = "//h3"
     headlines = driver.find_elements(By.XPATH, headline_element_XPATH)
 
-    print("Top Headlines:\n")
+    print(f"Headlines({len(headlines)}):\n")
     for h in headlines:
         print("-", h.text)
 
